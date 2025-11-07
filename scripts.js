9affa792accc80a5ea06214dcb8c57821ab41e16
YanchikSivova <sivova0108@gmail.com>
2025-11-07 10:22:31 +0300

    feat: interative nav add

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     let cards = document.querySelectorAll('.course-card');
-    let activeСard = document.querySelector('.active-card');
+    let activeCard = document.querySelector('.active-card');
 
     function setActiveCard(clickedCard){
         if(clickedCard.classList.contains('active-card')){
@@ -11,12 +11,30 @@ document.addEventListener('DOMContentLoaded', function(){
         });
 
         clickedCard.classList.add('active-card');
-        activeСard = clickedCard;
+        activeCard = clickedCard;
     }
 
     cards.forEach(card=>{
         card.addEventListener('click', function(){
             setActiveCard(this);
+        })
+    })
+
+    let navItems = document.querySelectorAll('.nav__item');
+    let activeItem = document.querySelector('.nav__item--active');
+    function setActiveNav(clickedNav){
+        if(clickedNav.classList.contains('nav__item--active')){
+            return;
+        };
+        navItems.forEach(item=>{
+            item.classList.remove('nav__item--active');
+        });
+        clickedNav.classList.add('nav__item--active');
+        activeItem = clickedNav;
+    }
+    navItems.forEach(item=>{
+        item.addEventListener('click', function(){
+            setActiveNav(this);
         })
     })
 });
