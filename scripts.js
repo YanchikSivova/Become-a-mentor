9b68cf93fdd1214dc94b7684c22fd3e48b55c1f0
YanchikSivova <sivova0108@gmail.com>
2025-11-08 14:09:54 +0300

    feat: add adaptive footer

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
+    
+    // КАРТОЧКИ
     let cards = document.querySelectorAll('.course-card');
     let activeCard = document.querySelector('.active-card');
 
@@ -20,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     })
 
+    // НАВИГАЦИЯ
     let navItems = document.querySelectorAll('.nav__item');
     let activeItem = document.querySelector('.nav__item--active');
     function setActiveNav(clickedNav){
@@ -35,6 +38,26 @@ document.addEventListener('DOMContentLoaded', function(){
     navItems.forEach(item=>{
         item.addEventListener('click', function(){
             setActiveNav(this);
+        })
+    })
+
+    // FOOTER
+    const linksHeaders = document.querySelectorAll('.links__header-wrapper');
+    linksHeaders.forEach(header =>{
+        header.addEventListener('click', function(){
+            if(window.innerWidth <=2024){
+                const linksList = this.nextElementSibling;
+                const toggleBtn = this.querySelector('.links__toggle');
+
+                document.querySelectorAll('.links-list.active').forEach(activeList=>{
+                    if(activeList!==linksList){
+                        activeList.classList.remove('active');
+                        activeList.previousElementSibling.querySelector('.links__toggle').classList.remove('rotated');
+                    }
+                });
+                linksList.classList.toggle('active');
+                toggleBtn.classList.toggle('rotated');
+            }
         })
     })
 });
